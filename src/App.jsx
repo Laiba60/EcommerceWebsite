ac851a06be658751fb11fdcc5cb0fee1ae65895c
Laiba60 <laibasaeed026@gmail.com>
2026-03-02 08:49:17 +0500

    Set the files and Folders in the Componnets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { use } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect,useState } from 'react'
 
 const App = () => {
+  const [orderPopup,setOrderPopup] = useState(false)
+  const handleordrPopup = () => {
+    setOrderPopup(!orderPopup)
+  }
+  useEffect(() => {
+    Aos.init({duration:2000,
+      offset:100,
+      easing:"ease-in-sine",
+      delay:100,
+
+    })
+    Aos.refresh()
+  },[])
   return (
-    <div>App</div>
+   <>
+   <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+
+   </div>
+   </>
   )
 }
 
